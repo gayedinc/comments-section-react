1eb2a045cbea4a5ffb84b4e465fe54ed40b8b26f
[Gaye Dinç] <gayedinc190@gmail.com>
2025-01-20 12:13:07 +0300

    yorum ve cevapları basıldı

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,30 @@ export default function App() {
         <button type="submit">Submit</button>
       </form>
       <div className="comment-section">
-        <h2>{data.name}</h2>
+        {comments.length === 0 ?
+          <p>No comments found.</p> :
+          <ul>
+            {comments.map((x, i) => <li key={i}>
+              <h2>{x.name}</h2>
+              <span>{x.time}</span>
+              <p>{x.comment}</p>
+              <span>Likes: {x.likes}</span>
+              <span>Dislikes: {x.dislikes}</span>
+              {x.replies && x.replies.length > 0 && (
+                <ul>
+                  {x.replies.map((reply, i) => (
+                    <li key={i}>
+                      <h3>{reply.name}</h3>
+                      <span>{reply.time}</span>
+                      <p>{reply.comment}</p>
+                      <span>Likes: {reply.likes}</span>
+                      <span>Dislikes: {reply.dislikes}</span>
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </li>)}
+          </ul>}
       </div>
     </>
   )
